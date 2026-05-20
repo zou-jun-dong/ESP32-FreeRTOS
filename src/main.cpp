8399520861faaf7e88871014c50873583cd302bb
zou-jun-dong <20250070118@stu.fosu.edu.cn>
2026-05-20 17:32:15 +0800

    feat(rtos): implement deferred interrupt processing via binary semaphore
    - Replaced SemaphoreCreateBinary with the modern standard xSemaphoreCreateBinary.
    - Implemented utton_isr with IRAM_ATTR and context-safe xSemaphoreGiveFromISR API.
    - Created high-priority Task_ButtonHandler using xSemaphoreTake with portMAX_DELAY for zero-CPU idling.
    - Attached hardware interrupt to GPIO 0 for real-time falling-edge detection.
    - Documented the deferred interrupt architecture for maintainability.

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,65 @@
 #include <Arduino.h>
-typedef struct 
-{
-  uint32_t counter;
-  uint8_t statusCode;
-  uint8_t breathLightState;
-}DataPacket;
-QueueHandle_t xDataQueue;
+const int BUTTON_PIN=0;  //Define button pin,BOOT button is usually on GPIO 0
+SemaphoreHandle_t xButtonSemaphore=NULL;   //Declare a binary semaphore handle
 
-//Task: MQTT Data Publishing
-void Task_Sender(void* pvParameters)
+/**
+ * Interrupt Service Rountine-ISR
+ * IRAM_ATTR forces this function to run from RAM for maximum speed
+ */
+void IRAM_ATTR button_isr() {
+  //In a interrupt context,must use APIs with the 'FromISR' suffix!
+  BaseType_t xHigherPriorityTaskWoken=pdFALSE;
+  xSemaphoreGiveFromISR(xButtonSemaphore,&xHigherPriorityTaskWoken);  //Give the semaphore
+  //Force a context switch if a higher priority task was woken
+  if (xHigherPriorityTaskWoken==pdTRUE)
+  {
+    portYIELD_FROM_ISR();
+  }
+  
+}
+//Task: Receives semaphore and executes business logic
+void Task_ButtonHandler(void* pvParameters)
 {
-  DataPacket packetToSend;
-   packetToSend.counter=0;
-   packetToSend.statusCode=0;
-   packetToSend.breathLightState=0;
+ uint32_t pressCount=0;
  for (;;)
  {
-   packetToSend.counter++;
-   packetToSend.breathLightState=(packetToSend.breathLightState+15)%255;
-   BaseType_t xStatus=xQueueSend(xDataQueue,&packetToSend,(TickType_t)10);
-   if (xStatus==pdPASS)
+  //Block and wait for the semaphore
+  //portMAX_DELAY means waiting indefinitely without consuming CPU
+   if (xSemaphoreTake(xButtonSemaphore,portMAX_DELAY))
    {
-    Serial.printf("[Sender] ENQUEUED -> Count: %lu | Status: %d | Light PWM: %d\n", packetToSend.counter, packetToSend.statusCode, packetToSend.breathLightState);
-   }else{
-    Serial.println("[Sender] WARNING: Queue is full! Data dropped.");
+    pressCount++;
+    Serial.println("=================================");
+    Serial.printf("[Handler Task] Button Pressed!Count: %lu\n",pressCount);
+    Serial.printf("[Handler Task] Core ID: %d\n",xPortGetCoreID());
+    Serial.println("=================================");
+    vTaskDelay(200/portTICK_PERIOD_MS);
    }
-   vTaskDelay(1000/portTICK_PERIOD_MS);
  }
 }
 
-void Task_Receiver(void *pvParameters)
-{
-  DataPacket receivedPacket;
-  for (;;)
-  {
-    BaseType_t xStatus=xQueueReceive(xDataQueue,&receivedPacket,pdMS_TO_TICKS(2000));
-    if (xStatus==pdPASS)
-    {
-      Serial.printf("[Receiver] DEQUEUED <- Count: %lu | Status: %d | Light PWM: %d\n",receivedPacket.counter,receivedPacket.statusCode,receivedPacket.breathLightState);
-    }else{
-      Serial.println("[Receiver] INFO: Waiting for data timeout. Queue is currently empty.");
-    }
-  }
-}
 
 void setup() {
   Serial.begin(115200);
   //Wait for Serial stability
   vTaskDelay(1000/portTICK_PERIOD_MS);
-  //Initialize WiFi and MQTT
-  Serial.println("--- FreeRTOS Producer-Consumer Queue Test ---");
-  xDataQueue=xQueueCreate(10,sizeof(DataPacket));
-  if (xDataQueue!=NULL)
+
+  Serial.println("--- FreeRTOS Semaphore Interrupt Test ---");
+  /**
+   * Initialize Binary Semaphore
+   * A newly creater binary semaphore is 'empty' by default.
+   */
+  xButtonSemaphore=xSemaphoreCreateBinary();
+  if (xButtonSemaphore!=NULL)
   {
-    
-  xTaskCreatePinnedToCore(
-    Task_Sender,
-    "SenderTask",
-    2048,
-    NULL,
-    1,
-    NULL,
-    1
-  );
-
-  xTaskCreatePinnedToCore(Task_Receiver,"RecievedTask",2048,NULL,1,NULL,0);
-
-}else{
-  Serial.println("System] FATAL ERROR: Failed to create queue.");
-}
-
+    //Create task with high priority to ensure real-time responsiveness
+    xTaskCreatePinnedToCore(Task_ButtonHandler,"btnTask",2048,NULL,3,NULL,1); 
+  
+  pinMode(BUTTON_PIN,INPUT_PULLUP);  //Configure button pin as pull-up input
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN),button_isr,FALLING);
+  Serial.println("[System] Semaphore created and interrupt attached.");
+  }else{
+    Serial.println("[System] FATAL ERROR; Semaphore creation failer.");
+  }
 }
 
 void loop() {
